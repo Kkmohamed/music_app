65edeb2db425b825170839c51cfb4261e8e4b906
MohamedAli <midos123rrr@gmail.com>
2026-03-24 01:44:03 +0200

    add font orbitron

--- lib/core/resources/app_assets.dart
+++ lib/core/resources/app_assets.dart
@@ -1,4 +1,6 @@
 class AppAssets {
+  //fonts
+  static const String orbitron = 'Orbitron';
   //images
   //splash
   static const String logo = 'assets/images/logo.png';
--- lib/features/on_boarding/widgets/onboarding_body.dart
+++ lib/features/on_boarding/widgets/onboarding_body.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/core/resources/app_assets.dart';
+import 'package:music_app/core/resources/app_colors.dart';
 
 class OnboardingBody extends StatelessWidget {
   const OnboardingBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [AppColors.kPrimaryColor, AppColors.kSecondPrimaryColor],
+        ),
+      ),
+      child: Center(
+        child: Text(
+          'data',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: AppAssets.orbitron,
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
   }
 }
